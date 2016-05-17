5669cfb5f94ce830ee4c7d7d38aec81ee0ef7a58
mithun3010 <mithun3010@gmail.com>
2016-05-17 12:05:17 +0100

    Add files via upload

---
+++ Bluetooth Voice Control/blue_voice/blue_voice.ino
@@ -0,0 +1,33 @@
+String voice;
+int led1 = 13; 
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  pinMode(led1, OUTPUT);
+}
+
+void loop() {
+  while (Serial1.available()){  
+  delay(10); 
+  char c = Serial1.read(); 
+  if (c == '#') {
+  break;
+  } 
+  voice += c; 
+  } 
+  if (voice.length() > 0) {
+    Serial1.println(voice);
+
+   if(voice == "*lock on") 
+   {
+   digitalWrite(led1, HIGH);
+   }
+  else if(voice == "*lock off")
+  {
+  digitalWrite(led1, LOW);
+  }
+
+voice="";
+  }
+} 
