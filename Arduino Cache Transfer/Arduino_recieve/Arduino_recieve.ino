3a3d2850d2bf21eabf486cff997c771199a97793
mithun3010 <mithun3010@gmail.com>
2016-05-17 12:05:05 +0100

    Add files via upload

---
+++ Arduino Cache Transfer/Arduino_recieve/Arduino_recieve.ino
@@ -0,0 +1,28 @@
+#include <EasyTransfer.h>
+ 
+EasyTransfer ET;
+ 
+struct DATA_STRUCTURE{
+int uid_data;
+char ndef_data;
+};
+ 
+DATA_STRUCTURE cache;
+ 
+void setup()
+{
+ Serial.begin(9600);
+ ET.begin(details(cache), &Serial);
+}
+ 
+void loop()
+{
+
+ if(ET.receiveData())
+ {
+Serial.println(cache.uid_data);
+Serial.println(cache.ndef_data);
+delay(500);
+ }
+ 
+}
---
+++ Arduino Cache Transfer/Arduino_transfer/Arduino_transfer.ino
@@ -0,0 +1,30 @@
+#include <EasyTransfer.h>
+ 
+EasyTransfer ET;
+
+ 
+struct DATA_STRUCTURE{
+int uid_data;
+char ndef_data;
+};
+ 
+DATA_STRUCTURE cache;
+ 
+int counter;
+ 
+void setup()
+{
+
+ Serial.begin(9600);
+ ET.begin(details(cache), &Serial);
+ 
+counter = 0;
+}
+ 
+void loop()
+{
+cache.uid_data = counter;
+cache.ndef_data = counter++;
+ET.sendData();
+delay(1000);
+}
