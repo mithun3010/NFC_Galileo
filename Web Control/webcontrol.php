3e8bb856853f701ebd58fb5986d149f79e007332
mithun3010 <mithun3010@gmail.com>
2016-05-17 12:05:49 +0100

    Add files via upload

---
+++ Web Control/webcontrol.php
@@ -0,0 +1,19 @@
+﻿<?php 
+
+if (isset($_GET['action'])) { 
+require("php_serial.php");
+$serial = new phpSerial();
+$serial->deviceSet("COM7");
+$serial->confBaudRate(9600);
+$serial -> deviceOpen();
+
+if ($_GET[’action’] == "lock") {
+$serial->sendMessage("1\r"); 
+} else if ($_GET[’action’] == "unlock") {
+$serial->sendMessage("0\r");
+}
+
+$serial->deviceClose();
+} 
+
+?>
